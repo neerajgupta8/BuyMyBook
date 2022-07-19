ebba7fc5c9657a6ea9340d21594e3f982eb82bad
Neeraj <neerajgupta8524@gmail.com>
2022-07-19 14:39:35 +0530

    Title changed

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -5,8 +5,8 @@ import { signin, authenticate, isAuthenticated } from "../auth";
 
 const Signin = () => {
   const [values, setValues] = useState({
-    email: "neeraj@gmail.com",
-    password: "nnnnnn9",
+    email: "",
+    password: "",
     error: "",
     loading: false,
     redirectToReferrer: false,
